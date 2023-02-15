5f54dc7cabb5deb8e361625201b7858c58c41acd
Rafisto <1.rafal.wlodarczyk@gmail.com>
2023-02-15 21:44:37 +0100

    Videoplayer changes

--- videoplayer/videoplayer.py
+++ videoplayer/videoplayer.py
@@ -1,10 +1,16 @@
 import argparse
 import os
+import cv2
+import numpy
 
 argument_parser = argparse.ArgumentParser(description="Terminal Video Player",
                                           formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-argument_parser.add_argument("file", help="Video file to play")
+argument_parser.add_argument("-f", "--file", help="Video file to play")
+argument_parser.add_argument("-c", "--color", help="Color mode", action='store_true')
 argument_parser.usage = "videoplayer.py [-h for help]"
+
+pixel_set = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+play_color = False
 
 
 def CheckFileExists(file):
@@ -13,17 +19,56 @@ def CheckFileExists(file):
     return file
 
 
+
 def CheckHelp(parameters):
-    if parameters.help:
+    if 'help' in parameters:
         argument_parser.print_help()
         exit()
 
 
-class VideoPlayer:
-    parameters = argument_parser.parse_args()
-    CheckHelp(parameters)
-
-    file = CheckFileExists(parameters.file)
+def MapFrameToTerminal(frame):
+    c, l = os.get_terminal_size().columns, os.get_terminal_size().lines
+    return cv2.resize(frame, (c, l))
 
 
-VideoPlayer()
+def PixelToAscii(pixel):
+    return pixel_set[int((len(pixel_set) - 1) * (1 - numpy.average(pixel) / 255))]
+
+
+def PixelToAsciiColor(pixel):
+    return f"\033[38;2;{pixel[2]};{pixel[1]};{pixel[0]}m{pixel_set[int((len(pixel_set) - 1) * (1 - numpy.average(pixel) / 255))]}"
+
+
+def MapIntensity(frame):
+    global play_color
+    if play_color:
+        return ''.join([''.join([PixelToAsciiColor(p) for p in pixel]) for pixel in frame])
+    return ''.join([''.join([PixelToAscii(p) for p in pixel]) for pixel in frame])
+
+
+def PlayVideoFrames(file):
+    video = cv2.VideoCapture(file)
+    while video.isOpened():
+        ret, frame = video.read()
+        if ret:
+            frame = MapFrameToTerminal(frame)
+            frame = MapIntensity(frame)
+            print("".join(frame))
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+
+parameters = argument_parser.parse_args()
+CheckHelp(parameters)
+
+if os.name == 'nt' and 'color' in parameters:
+    print("Color mode probably not supported on Windows, consider switching to better OS")
+if 'color' in parameters:
+    play_color = parameters.color
+
+file = CheckFileExists(parameters.file)
+PlayVideoFrames(file)
