cf0600fc144a70927553b0fc93a1c568d4288f6b
SmigorX <igorjaniak@gmail.com>
2023-02-16 23:26:57 +0100

    PreviewFramerate allows user to set playback FPS If not specified falls back to FPS of original video

--- videoplayer/videoplayer.py
+++ videoplayer/videoplayer.py
@@ -11,7 +11,7 @@ argument_parser.add_argument("-f", "--file", help="Video file to play")
 argument_parser.add_argument("-c", "--color", help="Color mode", action='store_true')
 argument_parser.add_argument("-p", "--pre", help="Pre-render video", action='store_true')
 argument_parser.add_argument("-s", "--save", help="Save pre-rendered video", action='store_true')
-argument_parser.add_argument("-F", "--fps", help="Framerate of the video", type=int)
+argument_parser.add_argument("-F", "--fps", help="Framerate of the video", type=int, default=0)
 argument_parser.usage = "videoplayer.py [-h for help]"
 
 pixel_set = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
@@ -50,22 +50,27 @@ def MapIntensity(frame):
     return ''.join([''.join([PixelToAscii(p) for p in pixel]) for pixel in frame])
 
 
-def OriginalFramerate(file):
-    video = cv2.VideoCapture(file)
-    fps = video.get(cv2.CAP_PROP_FPS)
+def PreviewFramerate(file):
+    parameters = argument_parser.parse_args()
+    if parameters.fps != 0:
+        ManualFPS = parameters.fps
+        return ManualFPS
+    else:
+        video = cv2.VideoCapture(file)
+        SourceFPS = video.get(cv2.CAP_PROP_FPS)
+        return SourceFPS
     video.release()
-    return fps
-
 
 def PlayVideoFrames(file):
     video = cv2.VideoCapture(file)
+    FrameSleep = ((1 / PreviewFramerate(file)) * 0.564)
     while video.isOpened():
         ret, frame = video.read()
         if ret:
             frame = MapFrameToTerminal(frame)
             frame = MapIntensity(frame)
             print("".join(frame))
-            time.sleep(1 / OriginalFramerate(file) - 0.0134)
+            time.sleep(FrameSleep)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
